0cb9e71a550ef1aeff074b34ad9f4d310caa8e8b
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-11 01:44:49 +0100

    :sparkles: Set up middleware and stuff

--- async-redux/src/App.js
+++ async-redux/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import thunk from 'redux-thunk';
 import * as reducers from "./state/reducer";
-import { createStore } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
-import { combineReducers } from "redux";
 
 import './App.css';
 import Counter from './components/Counter';
@@ -17,7 +17,11 @@ function App() {
   
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    {},
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),
   );
 
   // console.log(store);
